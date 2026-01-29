a66f8b53f5710f56e2cab50ffa9a2b8a79c67e4f
Deshaun Brown <deshaunbrown933@yahoo.com>
2026-01-29 15:09:57 -0600

    Final Project Chapter 3 Landing Page

---
+++ Final-Project/Chapter 3 assignment Landing Page/Landing Page/Program.cs
@@ -0,0 +1,27 @@
+using Landing_Page.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register MVC controller services
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Page}/{action=Index}/{id?}");
+
+app.Run();
--- Labs/CH4/Chapter 4/Chapter 4-1 Project/Program.cs
+++ Labs/CH4/Chapter 4/Chapter 4-1 Project/Program.cs
@@ -1,6 +1,27 @@
+using Chapter_4_1_Project.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register MVC controller services
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Page}/{action=Index}/{id?}");
 
 app.Run();
